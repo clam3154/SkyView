370a310ab3dfc72c20272adde9794b50b3224a7d
clam3154 <clam3154@bths.edu>
2018-11-27 10:15:19 -0500

    so far

--- src/SkyView.java
+++ src/SkyView.java
@@ -3,26 +3,29 @@ public class SkyView
     private double[][] view;
     public SkyView(int numRows, int numCols, double[] scanned)
     {
+        view = new double[numRows][numCols];
         int num = 0;
-        for(int i = 0; i < numRows; i++)
+        for (int i = 0; i < numRows; i++)
         {
-            if((i%2) == 0)
+            if ((i % 2) == 0)
             {
-                for(int j = 0; j < numCols; j++)
+                for (int j = 0; j < numCols; j++)
                 {
                     view[i][j] = scanned[num];
                     num++;
+                    System.out.print(view[i][j] + " ");
                 }
-            }
-            else
-            {
-                for (int j = numCols-1; j > 0; j--)
+            } else {
+                for (int j = numCols - 1; j > 0; j--)
                 {
                     view[i][j] = scanned[num];
                     num++;
+                    System.out.print(view[i][j] + " ");
                 }
             }
+            System.out.println();
         }
+        System.out.println();
     }
 
     public double getAverage(int startRow, int endRow, int startCol, int endCol)
@@ -31,10 +34,21 @@ public class SkyView
         double total = 0;
         for(int i = startRow; i < endRow; i++)
         {
-            for(int j = startCol; j < endCol; j++)
+            if((i%2) == 0)
             {
-                total += view[i][j];
-                length++;
+                for(int j = startCol; j < endCol; j++)
+                {
+                    total += view[i][j];
+                    length++;
+                }
+            }
+            else
+            {
+                for (int j = endCol-1; j > 0; j--)
+                {
+                    total += view[i][j];
+                    length++;
+                }
             }
         }
         return  total/length;
